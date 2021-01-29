328b936b9bd933e5e16e59704a02db5d21f84d42
Jason-fors <jasonfors@hotmail.com>
2021-01-28 22:37:50 -0500

    Update Basic_HTML_and_JavaScript_1 files.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("This JavaScript alert lives in a different file.");
+
+function My_First_Function() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
